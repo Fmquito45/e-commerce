24a3cd760ce3c233e212255e611c95fbe07e181a
Fmquito45 <fmquitox45@gmail.com>
2023-10-12 18:45:30 -0400

    creates get route for single id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,9 +16,15 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
   // be sure to include its associated Category and Tag data
+  const product = await Product.findByPk(req.params.id, {include: [{ model: Category}, { model: Tag}]});
+  !product ? res.status(500).json(err) : res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
